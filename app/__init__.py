8a6f2269fb2c7e3e3950cb608625c6c9ba145c7b
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-11-12 13:48:49 +0530

    feat: Modify imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
-from app.utils.chat import chat_chain
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from app.utils.llm import llm
+from app.utils.chat import chat_chain
 from app.utils.models import (AddCategory, Category, CategoryExample,
                               DeleteCategory)
 from app.utils.mongodb_connection import MongoDB
