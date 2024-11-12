ce2ad457d5512d24c5237a1a00cb8e90742865e7
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-11-12 09:35:59 +0530

    feat: Add support for huggingface embeddings

--- app/utils/llm.py
+++ app/utils/llm.py
@@ -1,16 +1,19 @@
-from langchain_ollama.llms import OllamaLLM
-from langchain_ollama.embeddings import OllamaEmbeddings
-from langchain_google_genai import ChatGoogleGenerativeAI
 from os import getenv
 
 PROVIDER = getenv("LLM_PROVIDER")
 OLLAMA_URL = getenv("OLLAMA_URL")
 
 if PROVIDER == "ollama":
+    from langchain_ollama.llms import OllamaLLM
+    from langchain_ollama.embeddings import OllamaEmbeddings
+    
     llm = OllamaLLM(base_url=OLLAMA_URL, model="phi3:latest")
     embeddings = OllamaEmbeddings(base_url=OLLAMA_URL, model="nomic-embed-text")
     
 elif PROVIDER == "gemini":
+    from langchain_google_genai import ChatGoogleGenerativeAI
+    from langchain_huggingface import HuggingFaceEmbeddings
+
     llm = ChatGoogleGenerativeAI(
         model="gemini-1.5-flash-latest",
         api_key=getenv("GOOGLE_API_KEY"),
@@ -20,6 +23,10 @@ elif PROVIDER == "gemini":
         max_retries=2,
     )
 
+    embeddings_model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+
+
 if __name__ == "__main__":
-    # print(len(embeddings.embed_query("Hi")))
+    print(len(embeddings.embed_query("Hi")))
     print(llm.invoke("Hi"))
